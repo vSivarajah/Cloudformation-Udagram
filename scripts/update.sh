eaf5dc9a7484e6dfea4a84c8eaad36fce882dd71
Vignesh  Sivarajah <vigneshsivarajah@Vigneshs-MacBook-Pro.local>
2020-06-10 12:43:12 +0200

    added autoscaler group

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,4 +2,5 @@
 
 aws cloudformation update-stack --stack-name $1 \
  --template-body file://$2 --parameters file://$3 \
- --region us-west-2ls
+ --capabilities CAPABILITY_IAM \
+ --region us-west-2
